e75fe7c2aa87cbeaaf0d73ed558c414791fbc183
georgeee@126.com <georgeee@126.com>
2019-03-19 17:34:51 +0800

    modify obtain audiopath

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
+#include <stdlib.h>
+#include <string>
 #include "GetFileInfo.h"
 using namespace std;
 
-void getInfo();
+void getInfo(char *);
 
-int main(){
-	getInfo();
+int main(int args, char *argv[]){
+	getInfo(argv[0]);
 	return 0;
 }
 
-void getInfo(){
-	char *path = "/home/jx/code/minegithub/ffmpegtest1/1.mp3";
-	read_av_info(path);
+void getInfo(char *projectPath){
+	string *audioPath = new string(projectPath);
+	*audioPath = audioPath->substr(0, audioPath->find("/Debug"));
+	audioPath->append("/1.mp3");
+	read_av_info(audioPath->c_str());
+	delete audioPath;
 }
