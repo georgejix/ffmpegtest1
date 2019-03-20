210dd485fa7837c7467dcf6a42e838a5427622eb
georgeee@126.com <georgeee@126.com>
2019-03-20 10:03:00 +0800

    /

--- src/Main.cpp
+++ src/Main.cpp
@@ -13,8 +13,8 @@ int main(int args, char *argv[]){
 
 void getInfo(char *projectPath){
 	string *audioPath = new string(projectPath);
-	*audioPath = audioPath->substr(0, audioPath->find("/Debug"));
-	audioPath->append("/1.mp3");
+	string deletePath = "/Debug/ffmpegtest1";
+	*audioPath = audioPath->replace(audioPath->find(deletePath), deletePath.length(), "/1.mp3");
 	read_av_info(audioPath->c_str());
 	delete audioPath;
 }
